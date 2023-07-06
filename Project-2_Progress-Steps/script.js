71ed2f849e3fccbc45efd9380c0b37934ac9a993
chandan135 <chesschan135@gmail.com>
2023-07-06 13:11:48 +0530

    mini-Project:Progress-steps

---
+++ Project-2_Progress-Steps/script.js
@@ -0,0 +1,47 @@
+"use strict";
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+next.addEventListener("click", function () {
+  currentActive++;
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  updateBar();
+});
+
+prev.addEventListener("click", function () {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  updateBar();
+});
+
+function updateBar() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const actives = document.querySelectorAll(".active");
+  //console.log(actives.length);
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === 4) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}
